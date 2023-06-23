d8f16c4c4ed87c9556490629e9cd5e3e1093c65a
Ilya <kozlov.ilya.code@gmail.com>
2023-06-23 11:32:45 +0300

    Add toggle buttons section to GUI

--- src/blocks/form-elements-section/__fields/form-elements-section__fields.js
+++ src/blocks/form-elements-section/__fields/form-elements-section__fields.js
@@ -3,3 +3,4 @@ import '../../dropdown/dropdown';
 import '../../date-range-picker/date-range-picker';
 import '../../masked-text-field/masked-text-field';
 import '../../filter-date-dropdown/filter-date-dropdown';
+import '../../subscription-text-field/subscription-text-field';
--- src/blocks/form-elements-section/form-elements-section.js
+++ src/blocks/form-elements-section/form-elements-section.js
@@ -1,1 +1,2 @@
 import './__fields/form-elements-section__fields';
+import './__toggle-buttons/form-elements-section__toggle-buttons';
