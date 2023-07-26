9041aafea8ba62998ab41b884b276dfd99f23c7b
Ilya <kozlov.ilya.code@gmail.com>
2023-07-26 13:34:27 +0300

    Make date range picker store dates in data attributes

--- src/blocks/date-range-picker/date-range-picker.js
+++ src/blocks/date-range-picker/date-range-picker.js
@@ -1,48 +1,60 @@
-import moment from 'moment';
+import moment from "moment";
 
-import '../date-dropdown/date-dropdown';
-import '../range-calendar/range-calendar';
+import "../date-dropdown/date-dropdown";
+import "../range-calendar/range-calendar";
+
+function setDatePickerDateFromAttributes(datePicker, dateFrom) {
+  datePicker.dataset.dateFrom = dateFrom;
+}
+
+function setDatePickerDateToAttributes(datePicker, dateTo) {
+  datePicker.dataset.dateTo = dateTo;
+}
 
 function setDateDropdownSelectValue(dropdown, value) {
-  dropdown.querySelector('.date-dropdown__select').innerHTML = value;
+  dropdown.querySelector(".date-dropdown__select").innerHTML = value;
 }
 
 function resetDateDropdownValue(dropdown) {
-  dropdown.querySelector('.date-dropdown__select').innerHTML =
+  dropdown.querySelector(".date-dropdown__select").innerHTML =
     dropdown.dataset.defaultValue;
 }
 
 function resetDateDropdowns(datePicker) {
   const dropdownFrom = datePicker.querySelector(
-    '.date-range-picker__date-dropdown-from'
+    ".date-range-picker__date-dropdown-from"
   );
   const dropdownTo = datePicker.querySelector(
-    '.date-range-picker__date-dropdown-to'
+    ".date-range-picker__date-dropdown-to"
   );
   resetDateDropdownValue(dropdownFrom);
   resetDateDropdownValue(dropdownTo);
+  setDatePickerDateFromAttributes(datePicker, "");
+  setDatePickerDateToAttributes(datePicker, "");
 }
 
 function updateDatePickerDropdowns(datePicker) {
-  const calendar = datePicker.querySelector('.range-calendar');
+  const calendar = datePicker.querySelector(".range-calendar");
   const dropdownFrom = datePicker.querySelector(
-    '.date-range-picker__date-dropdown-from'
+    ".date-range-picker__date-dropdown-from"
   );
   const dropdownTo = datePicker.querySelector(
-    '.date-range-picker__date-dropdown-to'
+    ".date-range-picker__date-dropdown-to"
   );
   let calendarFrom = calendar.dataset.dateFrom;
   let calendarTo = calendar.dataset.dateTo;
 
   if (calendarFrom) {
-    calendarFrom = moment(calendarFrom).format('DD.MM.YYYY');
+    calendarFrom = moment(calendarFrom).format("DD.MM.YYYY");
     setDateDropdownSelectValue(dropdownFrom, calendarFrom);
+    setDatePickerDateFromAttributes(datePicker, calendarFrom);
   } else {
     resetDateDropdownValue(dropdownFrom);
   }
   if (calendarTo) {
-    calendarTo = moment(calendarTo).format('DD.MM.YYYY');
+    calendarTo = moment(calendarTo).format("DD.MM.YYYY");
     setDateDropdownSelectValue(dropdownTo, calendarTo);
+    setDatePickerDateToAttributes(datePicker, calendarTo);
   } else {
     resetDateDropdownValue(dropdownTo);
   }
@@ -50,56 +62,56 @@ function updateDatePickerDropdowns(datePicker) {
 
 /* Handlers */
 function handleDatePickerSelectClick(event) {
-  const datePickers = document.querySelectorAll('.date-range-picker');
-  if (event.target.closest('.date-range-picker__date-dropdown')) {
-    let datePicker = event.target.closest('.date-range-picker');
+  const datePickers = document.querySelectorAll(".date-range-picker");
+  if (event.target.closest(".date-range-picker__date-dropdown")) {
+    let datePicker = event.target.closest(".date-range-picker");
 
     /* Hide others date picker's poups */
     datePickers.forEach((e) => {
       if (!e.isEqualNode(datePicker))
-        e.classList.remove('date-range-picker_opened');
+        e.classList.remove("date-range-picker_opened");
     });
 
     /* Show clicked date picker's poups */
-    datePicker.classList.add('date-range-picker_opened');
+    datePicker.classList.add("date-range-picker_opened");
 
     return;
   }
 
-  if (!event.target.closest('.date-range-picker__popup')) {
+  if (!event.target.closest(".date-range-picker__popup")) {
     datePickers.forEach((e) => {
-      e.classList.remove('date-range-picker_opened');
+      e.classList.remove("date-range-picker_opened");
     });
   }
 }
 
 function handleDatePickerCalendarDayUnitBtnClick(event) {
   const datePicker = event.currentTarget;
-  if (event.target.closest('.day-unit-btn')) {
+  if (event.target.closest(".day-unit-btn")) {
     updateDatePickerDropdowns(datePicker);
   }
 }
 
 function handleDatePickerCalendarClearBtnClick(event) {
   const datePicker = event.currentTarget;
-  if (event.target.closest('.range-calendar__clear-button')) {
+  if (event.target.closest(".range-calendar__clear-button")) {
     resetDateDropdowns(datePicker);
   }
 }
 
 function handleDatePickerCalendarSubmitBtnClick(event) {
   const datePicker = event.currentTarget;
-  if (event.target.closest('.range-calendar__submit-button')) {
-    datePicker.classList.remove('date-range-picker_opened');
+  if (event.target.closest(".range-calendar__submit-button")) {
+    datePicker.classList.remove("date-range-picker_opened");
   }
 }
 
 /* Add handlers */
-document.addEventListener('click', handleDatePickerSelectClick);
+document.addEventListener("click", handleDatePickerSelectClick);
 
-const datePickers = document.querySelectorAll('.date-range-picker');
+const datePickers = document.querySelectorAll(".date-range-picker");
 datePickers.forEach((datePicker) => {
-  datePicker.addEventListener('click', handleDatePickerCalendarDayUnitBtnClick);
-  datePicker.addEventListener('click', handleDatePickerCalendarClearBtnClick);
-  datePicker.addEventListener('click', handleDatePickerCalendarSubmitBtnClick);
+  datePicker.addEventListener("click", handleDatePickerCalendarDayUnitBtnClick);
+  datePicker.addEventListener("click", handleDatePickerCalendarClearBtnClick);
+  datePicker.addEventListener("click", handleDatePickerCalendarSubmitBtnClick);
 });
