57b3b07fdb9a61202960cb09b77056b1e441b6ac
Ilya <kozlov.ilya.code@gmail.com>
2023-07-28 14:45:25 +0300

    Add registration card to GUI

---
+++ src/blocks/card-registration/card-registration.js
@@ -0,0 +1,28 @@
+import "../button/button";
+import "../radio-button/radio-button";
+import "../field/field";
+import "../masked-text-field/masked-text-field";
+import "../toggle/toggle";
+
+const signupCards = document.querySelectorAll(".card-registration");
+
+function isFormDataValid(signupCard) {
+  const signupForm = signupCard.querySelector(".card-registration__form");
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  console.log("Signed up!");
+}
+
+function handleLoginBtnClick(event) {
+  if (event.target.closest(".card-registration__login-btn")) {
+    console.log("To login!");
+  }
+}
+
+signupCards.forEach((signupCard) => {
+  const signupForm = signupCard.querySelector(".card-registration__form");
+  signupForm.addEventListener("submit", handleSubmit);
+  signupCard.addEventListener("click", handleLoginBtnClick);
+});
--- src/blocks/cards-section/cards-section.js
+++ src/blocks/cards-section/cards-section.js
@@ -1,3 +1,4 @@
 import "../card-room-search/card-room-search";
 import "../card-room-book/card-room-book";
+import "../card-registration/card-registration";
 import "../range-calendar/range-calendar";
