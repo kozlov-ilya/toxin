ec1ac57af24587a000551b30df2615ec11aeaf3f
Ilya <kozlov.ilya.code@gmail.com>
2023-08-09 14:20:49 +0300

    Add landing page

--- src/pages/landing-page/landing-page.js
+++ src/pages/landing-page/landing-page.js
@@ -11,4 +11,56 @@ function handleBurgerButtonClick(event) {
   page.classList.toggle("page_scroll_lock");
 }
 
+function makeSearchCardResponsive(card) {
+  card.classList.add("card_expanded_yes");
+  card
+    .querySelector(".card-room-search__search-btn")
+    .classList.add("button_size_expanded");
+  card
+    .querySelector(".dropdown__select")
+    .classList.add("field_size_responsive");
+  card.querySelectorAll(".form-unit").forEach((formUnit) => {
+    formUnit.classList.add("form-unit_size_responsive");
+  });
+  card.querySelectorAll(".date-dropdown__select").forEach((formUnit) => {
+    formUnit.classList.add("field_size_responsive");
+  });
+}
+
+function makeSearchCardStatic(card) {
+  card.classList.remove("card_expanded_yes");
+  card
+    .querySelector(".card-room-search__search-btn")
+    .classList.remove("button_size_expanded");
+  card
+    .querySelector(".dropdown__select")
+    .classList.remove("field_size_responsive");
+  card.querySelectorAll(".form-unit").forEach((formUnit) => {
+    formUnit.classList.remove("form-unit_size_responsive");
+  });
+  card.querySelectorAll(".date-dropdown__select").forEach((formUnit) => {
+    formUnit.classList.remove("field_size_responsive");
+  });
+}
+
+function updateSearchCard() {
+  const match = window.matchMedia("(max-width: 599px)");
+  const searchCards = page.querySelectorAll(".card-room-search");
+  if (match.matches) {
+    searchCards.forEach((card) => {
+      makeSearchCardResponsive(card);
+    });
+  } else {
+    searchCards.forEach((card) => {
+      makeSearchCardStatic(card);
+    });
+  }
+}
+
 burgerButton.addEventListener("click", handleBurgerButtonClick);
+
+window.addEventListener("resize", (event) => {
+  updateSearchCard();
+});
+
+updateSearchCard();
